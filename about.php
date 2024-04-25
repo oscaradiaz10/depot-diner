7a99b21f3bf05c34040c489807cb810099089e3d
oscaradiaz10 <oscar.di@me.com>
2024-04-25 16:34:22 -0500

    Additional CSS Changes

--- about.php
+++ about.php
@@ -4,12 +4,33 @@
     <title>Depot Diner - About Us</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="css/main.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <?php include ("includes/links.php"); ?>
 </head>
 <body>
+
+    <nav class="navbar navbar-expand-md bg-light sticky-top">
+        <div class="container">
+            <a class="navbar-brand" href="/index.php"><img src="media/Depot Diner.png" width="210px" alt="logo"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Our Menu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Job Opportunities</a>
+                    </li>
+                </ul>
+                <button class="btn btn-primary" type="button">Contact Us</button>
+            </div>
+        </div>
+    </nav>
 
     <div class="container-fluid">
         <div class="row align-items-center">
