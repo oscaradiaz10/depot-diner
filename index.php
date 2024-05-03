d37bc44aa53af2710e4fcc4d2ea51d7f62d7ff7d
oscaradiaz10 <oscar.di@me.com>
2024-05-02 23:38:55 -0500

    More changes in general

--- index.php
+++ index.php
@@ -52,15 +52,15 @@
 
     <div class="container food-cat spacer-top">
         <div class="row g-2">  
-            <div class="col-4">
+            <div class="col-lg-4 col-sm-12">
                 <h2 class="img-headers">Breakfast</h2>
                 <img class="img-columns" src="media/IMG_4795.jpeg" width="100%" alt="food 1">
             </div>
-            <div class="col-4">
+            <div class="col-lg-4 col-sm-12">
                 <h2 class="img-headers">Lunch</h2>
                 <img class="img-columns" src="media/IMG_4885.jpeg" width="100%" alt="food 2">
             </div>
-            <div class="col-4">
+            <div class="col-lg-4 col-sm-12">
                 <h2 class="img-headers">Dinner</h2>
                 <img class="img-columns" src="media/IMG_4656.jpeg" width="100%" alt="food 3">
             </div>
@@ -78,7 +78,7 @@
                     <p>(417) 271-1090</p>
                         <hr class="mt-5"/>
                     <h3>Follow Us</h3>
-                    <div class="btn-group mt-3">
+                    <div class="btn-group mt-2">
                         <button type="button" class="btn btn-primary"><a href="https://www.google.com" target="_blank">
                             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
                                 <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path>
@@ -100,6 +100,7 @@
             </div>
         </div>
     </div>
+
 
     <?php include ("includes/footer.php"); ?>
 
