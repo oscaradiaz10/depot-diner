5904a8ad4d045ee24dd470cedb76a68365095a35
oscaradiaz10 <oscar.di@me.com>
2024-12-08 23:56:24 -0600

    Update hours

--- index.php
+++ index.php
@@ -95,8 +95,8 @@
             <div class="row">
                 <div class="col-lg-4 col-sm-12">
                     <h3>Hours</h3>
-                    <p><strong>Monday - Thursday</strong><br />7:30 AM - 7:00 PM</p>
-                    <p><strong>Friday - Saturday</strong><br />7:30 AM - 8:00 PM</p>
+                    <p><strong>Monday - Saturday</strong><br />7:30 AM - 7:00 PM</p>
+                    <!-- <p><strong>Friday - Saturday</strong><br />7:30 AM - 8:00 PM</p> -->
                         <hr class="mt-4"/>
                     <h3>Contact</h3>
                     <p>(417) 271-1090</p>
